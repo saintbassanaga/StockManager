c22039040ed0145c05f6477ee365c651e13bd2ce
saintbassanaga <saintbassanaga01@gmail.com>
2024-09-06 23:05:52 +0100

    chore: Updating Repositories for Persistence
    - CategoryRepository
    - ProductRepository

--- src/main/java/tech/saintbassanaga/stockmanager/repositories/CategoryRepository.java
+++ src/main/java/tech/saintbassanaga/stockmanager/repositories/CategoryRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import tech.saintbassanaga.stockmanager.models.Category;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID>, JpaSpecificationExecutor<Category> {
+  Optional<List<Category>> findCategoriesByDesignationContaining(String name);
+  Optional<List<Category>> findCategoriesByProductsNotEmpty();
 }
--- src/main/java/tech/saintbassanaga/stockmanager/repositories/ProductRepository.java
+++ src/main/java/tech/saintbassanaga/stockmanager/repositories/ProductRepository.java
@@ -4,7 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import tech.saintbassanaga.stockmanager.models.Product;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
+  Optional<Product> findProductById(UUID uuid);
+  Optional<List<Product>> findDistinctByCategory_Id(UUID categoryId);
+  Optional<List<Product>> findDistinctByNameContaining(String name);
+  void deleteDistinctById(UUID uuid);
 }
